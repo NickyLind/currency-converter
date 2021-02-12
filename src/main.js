eca5dbbf19ca9a93a3709880892911be2ad1769f
Nick Lindau <nicholaithegreat@gmail.com>
2021-02-12 11:57:32 -0800

    add error statement to when a country code that doesn't exist is entered

--- src/main.js
+++ src/main.js
@@ -4,24 +4,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './../src/conversion.js';
 
-async function Convert()  {
+async function Convert() {
   let convert = await CurrencyExchange.aquireCurrency();
   if (convert.result) {
     let USD = $("#currency").val();
     let country = $("#country").val();
     let conversionCountry = convert.conversion_rates[country];
-    console.log(conversionCountry);
-    let rate =  parseFloat(USD * conversionCountry).toFixed(2);
-    console.log(rate);
-    $("#outputExchange").append(`Here is ${country}'s current exchange rate: ${conversionCountry}`);
-    $("#outputConvert").append(`Here is your USD converted to ${country}: ${rate}`);
+    if (conversionCountry === undefined) {
+      $("#outputExchange").append("please enter a valid country code");
+    } else {
+      let rate = parseFloat(USD * conversionCountry).toFixed(2);
+      console.log(rate);
+      $("#outputExchange").append(`Here is ${country}'s current exchange rate: ${conversionCountry}`);
+      $("#outputConvert").append(`Here is your USD converted to ${country}: ${rate}`);
+    }
   } else {
     $("#outputExchange").append(`there was an error: ${convert}`);
-  }  
+  }
 }
 
-$(document).ready(function()  {
-  $("#convert").click(function() {
+$(document).ready(function () {
+  $("#convert").click(function () {
     $("#outputExchange").empty();
     $("#outputconvert").empty();
     Convert();
