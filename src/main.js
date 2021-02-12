cd377b0032ca30c0dcf8df2c3522feaf3a07d094
Nick Lindau <nicholaithegreat@gmail.com>
2021-02-12 11:45:29 -0800

    add functionality to enter ANY curency code and display that countries exchange rate and USD conversion amount

--- src/main.js
+++ src/main.js
@@ -4,87 +4,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './../src/conversion.js';
 
-async function CAD()  {
+async function Convert()  {
   let convert = await CurrencyExchange.aquireCurrency();
   if (convert.result) {
     let USD = $("#currency").val();
-    let CAD = `${convert.conversion_rates.CAD}`;
-    let rate =  parseFloat(USD * CAD).toFixed(2);
-    $("#output").append(`Here is your USD converted to CAD: ${rate}`);
+    let country = $("#country").val();
+    let conversionCountry = convert.conversion_rates[country];
+    console.log(conversionCountry);
+    let rate =  parseFloat(USD * conversionCountry).toFixed(2);
+    console.log(rate);
+    $("#outputExchange").append(`Here is ${country}'s current exchange rate: ${conversionCountry}`);
+    $("#outputConvert").append(`Here is your USD converted to ${country}: ${rate}`);
   } else {
-    $("#output").append(`there was an error: ${convert}`);
-  }
+    $("#outputExchange").append(`there was an error: ${convert}`);
+  }  
 }
-
-async function EUR()  {
-  let convert = await CurrencyExchange.aquireCurrency();
-  if (convert.result) {
-    let USD = $("#currency").val();
-    let EUR = `${convert.conversion_rates.EUR}`;
-    let rate =  parseFloat(USD * EUR).toFixed(2);
-    $("#output").append(`Here is your USD converted to EUR: ${rate}`);
-  } else {
-    $("#output").append(`there was an error: ${convert}`);
-  }
-}
-
-async function ISK()  {
-  let convert = await CurrencyExchange.aquireCurrency();
-  if (convert.result) {
-    let USD = $("#currency").val();
-    let ISK = `${convert.conversion_rates.ISK}`;
-    let rate =  parseFloat(USD * ISK).toFixed(2);
-    $("#output").append(`Here is your USD converted to ISK: ${rate}`);
-  } else {
-    $("#output").append(`there was an error: ${convert}`);
-  }
-}
-
-async function JPY()  {
-  let convert = await CurrencyExchange.aquireCurrency();
-  if (convert.result) {
-    let USD = $("#currency").val();
-    let JPY = `${convert.conversion_rates.JPY}`;
-    let rate =  parseFloat(USD * JPY).toFixed(2);
-    $("#output").append(`Here is your USD converted to JPY: ${rate}`);
-  } else {
-    $("#output").append(`there was an error: ${convert}`);
-  }
-}
-
-async function DKK()  {
-  let convert = await CurrencyExchange.aquireCurrency();
-  if (convert.result) {
-    let USD = $("#currency").val();
-    let DKK = `${convert.conversion_rates.DKK}`;
-    let rate =  parseFloat(USD * DKK).toFixed(2);
-    $("#output").append(`Here is your USD converted to DKK: ${rate}`);
-  } else {
-    $("#output").append(`there was an error: ${convert}`);
-  }
-}
-
-
 
 $(document).ready(function()  {
-  $("#CAD").click(function() {
-    $("#output").empty();
-    CAD();
-  });
-  $("#EUR").click(function() {
-    $("#output").empty();
-    EUR();
-  });
-  $("#ISK").click(function() {
-    $("#output").empty();
-    ISK();
-  });
-  $("#JPY").click(function() {
-    $("#output").empty();
-    JPY();
-  });
-  $("#DKK").click(function() {
-    $("#output").empty();
-    DKK();
+  $("#convert").click(function() {
+    $("#outputExchange").empty();
+    $("#outputconvert").empty();
+    Convert();
   });
 });
