d913bc1ae4891f54b332f99abd717fbc6e7286ca
Nick Lindau <nicholaithegreat@gmail.com>
2021-02-12 10:17:46 -0800

    add canadian functionality to project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,38 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from './../src/conversion.js';
+
+async function CAD()  {
+  let convert = await CurrencyExchange.aquireCurrency();
+  if (convert.result) {
+    let USD = $("#currency").val();
+    let CAD = `${convert.conversion_rates.CAD}`;
+    let rate =  parseFloat(USD * CAD).toFixed(2);
+    $("#output").append(`Here is your USD converted to CAD: ${rate}`);
+  } else {
+    $("#output").append(`there was an error: ${convert}`);
+  }
+}
+
+
+
+$(document).ready(function()  {
+  $("#CAD").click(function() {
+    $("#output").empty();
+    CAD();
+  });
+  $("#EUR").click(function() {
+    $("#output").empty();
+  });
+  $("#ISK").click(function() {
+    $("#output").empty();
+  });
+  $("#JPY").click(function() {
+    $("#output").empty();
+  });
+  $("#DKK").click(function() {
+    $("#output").empty();
+  });
+});
