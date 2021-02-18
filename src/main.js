8ffff98ed4d00554232e1d56c62856e6705a4234
Nick Lindau <nicholaithegreat@gmail.com>
2021-02-17 17:00:27 -0800

    add proper api key error catching and remove console logs'

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import CurrencyExchange from './../src/conversion.js';
 
 async function Convert() {
   let convert = await CurrencyExchange.aquireCurrency();
-  if (convert.result) {
+  if (convert.result === "success") {
     let USD = $("#currency").val();
     let country = $("#country").val().toUpperCase();
     let conversionCountry = convert.conversion_rates[country];
@@ -14,31 +14,32 @@ async function Convert() {
       $("#outputExchange").append("please enter a valid country code");
     } else {
       let rate = parseFloat(USD * conversionCountry).toFixed(2);
-      console.log(rate);
       $("#outputExchange").append(`Here is ${country}'s current exchange rate: ${conversionCountry}`);
       $("#outputConvert").append(`Here is your USD converted to ${country}: ${rate}`);
     }
   } else {
-    $("#outputExchange").append(`there was an error: ${convert}`);
+    $("#outputExchange").append(`there was an error: ${convert["error-type"]}`);
   }
 }
 
 async function ListRates()  {
   let list = await CurrencyExchange.aquireCurrency();
   let allCountries = list.conversion_rates;
-  if  (list.result) {
+  if  (list.result === "success") {
     for (const [country, value] of Object.entries(allCountries)) {
       $("#list").append(`${country}: ${value}<br>`);
     }
   } else {
-    $("#list").append(`We are having trouble retrieving this information at this time ${list}`);
+    $("#list").append(`We are having trouble retrieving this information at this time. Error: "${list["error-type"]}"`);
   }
 }
 
 async function UpdateTime() {
   let time = await CurrencyExchange.aquireCurrency();
-  if (time.result)  {
-    $(".time").append(`${time.time_last_update_utc}`)
+  if (time.result === "success")  {
+    $(".time").append(`${time.time_last_update_utc}`);
+  } else {
+    $(".time").append(`There was an error processing this information. Error:  "${time["error-type"]}"`);
   }
 }
 
